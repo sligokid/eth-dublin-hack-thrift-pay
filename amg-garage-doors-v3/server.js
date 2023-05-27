8598e55878e33bcfc36595e63c7305712ba4a981
sligokid <kieranmcgowan@Kierans-M1-Mac.local>
2023-05-27 18:31:58 +0100

    project reorg

--- amg-garage-doors-v3/server.js
+++
@@ -1,7 +0,0 @@
-//server.js file
-var express = require('express')
-var app = express()
-app.use(express.static(__dirname)) //serves the static html file
-var server = app.listen(3001, () =>{
-console.log("server is listening on port", server.address().port);
-}) //gets express server started and listening for requests
